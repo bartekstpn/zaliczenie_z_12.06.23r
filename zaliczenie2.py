7cbd3e3d79403e3ffb26f8f7c614cef5b1697a4d
bartekstpn <bartekstpn@gmail.com>
2023-06-03 12:25:36 +0200

    dodano podstawowe wartosci

---
+++ zaliczenie2.py
@@ -0,0 +1,7 @@
+inflacja1 = 1.59282448436825
+inflacja2 = -0.453509101198007
+inflacja3 = 2.32467171712441
+kwota_kredytu = float(input("Podaj Kwotę kredytu: "))
+oprocentowanie = float(input("Podaj oprocentowanie: "))
+kwota_raty_kredytu = float(input("Podaj kwotę raty: "))
+
